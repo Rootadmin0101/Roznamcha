ad7aa06c6439ae890aa64dc56d7af98433d779fb
Rootadmin0101 <ayatebadi@gmail.com>
2026-06-25 07:32:29 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = 'hesab-v5-cache';
+const CACHE_NAME = 'hesab-app-v1';
 const ASSETS = [
+  './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.png' // اگر نام عکس آیکون شما چیز دیگری است، اینجا تغییر دهید
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+// نصب و ذخیره فایل‌ها در حافظه گوشی
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+// استفاده از فایل‌های ذخیره شده در زمان قطعی اینترنت
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// به‌روزرسانی حافظه در صورت تغییر کدها
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
